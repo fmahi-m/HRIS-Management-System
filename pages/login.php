e9864791c3d081cde854893def156df9c5c9408c
Fabiha Montaha Mahi <200460219+fmahi-m@users.noreply.github.com>
2026-08-07 03:34:26 +0600

    Create HRIS login page

---
+++ pages/login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>HRIS Login</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body class="bg-light">
+
+    <div class="container">
+
+        <div class="row justify-content-center mt-5">
+
+            <div class="col-md-4">
+
+                <div class="card p-4 shadow">
+
+                    <h2 class="text-center mb-4">HRIS Login</h2>
+
+                    <form action="" method="POST">
+
+                        <label>Username</label>
+                        <input type="text" class="form-control mb-3" name="username">
+
+                        <label>Password</label>
+                        <input type="password" class="form-control mb-3" name="password">
+
+                        <button type="submit" class="btn btn-primary w-100">
+                            Login
+                        </button>
+
+                    </form>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
